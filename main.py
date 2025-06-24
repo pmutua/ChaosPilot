99cf77c239340ad871faf52e3e712ec589cc30ad
Philip Mutua <pmutua@live.com>
2025-06-24 14:27:28 +0300

    fix:port number

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ app = get_fast_api_app(
 
 if __name__ == "__main__":
     # Use the PORT environment variable provided by Cloud Run, defaulting to 8080
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
